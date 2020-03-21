f9585b98c8e7986ef0f3e232f116eedfbffc9488
Federico Jimenez <fedeejimenez@gmail.com>
2020-03-21 10:21:15 +0000

    Get rid of most external js plugins and tricky functions to start fresh

--- app/assets/javascripts/plugins/blog.js
+++ app/assets/javascripts/plugins/blog.js
@@ -1,156 +1,156 @@
-/* JS Document */
+// /* JS Document */
 
-/******************************
+// /******************************
 
-[Table of Contents]
+// [Table of Contents]
 
-1. Vars and Inits
-2. Set Header
-3. Init Menu
-4. Init Header Search
-5. Init Masonry
+// 1. Vars and Inits
+// 2. Set Header
+// 3. Init Menu
+// 4. Init Header Search
+// 5. Init Masonry
 
 
-******************************/
+// ******************************/
 
-$(document).ready(function()
-{
-	"use strict";
+// $(document).ready(function()
+// {
+// 	"use strict";
 
-	/* 
+// 	/* 
 
-	1. Vars and Inits
+// 	1. Vars and Inits
 
-	*/
+// 	*/
 
-	var header = $('.header');
-	var menuActive = false;
-	var menu = $('.menu');
-	var burger = $('.hamburger');
+// 	var header = $('.header');
+// 	var menuActive = false;
+// 	var menu = $('.menu');
+// 	var burger = $('.hamburger');
 
-	setHeader();
+// 	setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+// 	$(window).on('resize', function()
+// 	{
+// 		setHeader();
+// 	});
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+// 	$(document).on('scroll', function()
+// 	{
+// 		setHeader();
+// 	});
 
-	initMenu();
-	initHeaderSearch();
-	initMasonry();
+// 	initMenu();
+// 	initHeaderSearch();
+// 	initMasonry();
 
-	/* 
+// 	/* 
 
-	2. Set Header
+// 	2. Set Header
 
-	*/
+// 	*/
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 100)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+// 	function setHeader()
+// 	{
+// 		if($(window).scrollTop() > 100)
+// 		{
+// 			header.addClass('scrolled');
+// 		}
+// 		else
+// 		{
+// 			header.removeClass('scrolled');
+// 		}
+// 	}
 
-	/* 
+// 	/* 
 
-	3. Init Menu
+// 	3. Init Menu
 
-	*/
+// 	*/
 
-	function initMenu()
-	{
-		if($('.menu').length)
-		{
-			var menu = $('.menu');
-			if($('.hamburger').length)
-			{
-				burger.on('click', function()
-				{
-					if(menuActive)
-					{
-						closeMenu();
-					}
-					else
-					{
-						openMenu();
+// 	function initMenu()
+// 	{
+// 		if($('.menu').length)
+// 		{
+// 			var menu = $('.menu');
+// 			if($('.hamburger').length)
+// 			{
+// 				burger.on('click', function()
+// 				{
+// 					if(menuActive)
+// 					{
+// 						closeMenu();
+// 					}
+// 					else
+// 					{
+// 						openMenu();
 
-						$(document).one('click', function cls(e)
-						{
-							if($(e.target).hasClass('menu_mm'))
-							{
-								$(document).one('click', cls);
-							}
-							else
-							{
-								closeMenu();
-							}
-						});
-					}
-				});
-			}
-		}
-	}
+// 						$(document).one('click', function cls(e)
+// 						{
+// 							if($(e.target).hasClass('menu_mm'))
+// 							{
+// 								$(document).one('click', cls);
+// 							}
+// 							else
+// 							{
+// 								closeMenu();
+// 							}
+// 						});
+// 					}
+// 				});
+// 			}
+// 		}
+// 	}
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
+// 	function openMenu()
+// 	{
+// 		menu.addClass('active');
+// 		menuActive = true;
+// 	}
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
+// 	function closeMenu()
+// 	{
+// 		menu.removeClass('active');
+// 		menuActive = false;
+// 	}
 
-	/* 
+// 	/* 
 
-	4. Init Header Search
+// 	4. Init Header Search
 
-	*/
+// 	*/
 
-	function initHeaderSearch()
-	{
-		if($('.search_button').length)
-		{
-			$('.search_button').on('click', function()
-			{
-				if($('.header_search_container').length)
-				{
-					$('.header_search_container').toggleClass('active');
-				}
-			});
-		}
-	}
+// 	function initHeaderSearch()
+// 	{
+// 		if($('.search_button').length)
+// 		{
+// 			$('.search_button').on('click', function()
+// 			{
+// 				if($('.header_search_container').length)
+// 				{
+// 					$('.header_search_container').toggleClass('active');
+// 				}
+// 			});
+// 		}
+// 	}
 
-	/* 
+// 	/* 
 
-	5. Init Masonry
+// 	5. Init Masonry
 
-	*/
+// 	*/
 
-	function initMasonry()
-	{
-		if($('.blog_post_container').length)
-		{
-			$('.blog_post_container').masonry(
-			{
-				itemSelector:'.blog_post',
-				columnWidth:'.blog_post',
-				gutter:30
-			});
-		}
-	}
+// 	function initMasonry()
+// 	{
+// 		if($('.blog_post_container').length)
+// 		{
+// 			$('.blog_post_container').masonry(
+// 			{
+// 				itemSelector:'.blog_post',
+// 				columnWidth:'.blog_post',
+// 				gutter:30
+// 			});
+// 		}
+// 	}
 
-});
+// });

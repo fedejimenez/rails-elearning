941cdbbe3feb48c11c71f8b511d8f9dd67be6e53
Federico Jimenez <fedeejimenez@gmail.com>
2020-03-21 22:38:52 +0000

    Add gem for breadcrumbs

--- Gemfile
+++ Gemfile
@@ -50,6 +50,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 # Fontawesome
 gem "font-awesome-rails"
 
+# Breadcrumbs
+gem "breadcrumbs_on_rails"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  add_breadcrumb 'Home', :root_path
+
   def index
+    add_breadcrumb 'Home', :root_path
     # @posts = Post.limit(5)
   end
-
+  
   def about
+    add_breadcrumb 'About', :about_path
   end
-
+  
   def courses
+    add_breadcrumb 'Courses', :courses_path
   end
 end
